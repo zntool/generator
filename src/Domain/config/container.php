8000129a5a1da72d7cb75ff43fb6432c4b166121
vitaliy <theyamshikov@yandex.ru>
2021-03-20 19:49:29 +0200

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -5,6 +5,7 @@ return [
 
     ],
     'singletons' => [
-
+        \ZnTool\Generator\Domain\Interfaces\Services\DomainServiceInterface::class => \ZnTool\Generator\Domain\Services\DomainService::class,
+        \ZnTool\Generator\Domain\Interfaces\Services\ModuleServiceInterface::class => \ZnTool\Generator\Domain\Services\ModuleService::class,
     ],
 ];
